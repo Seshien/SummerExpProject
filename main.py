981aecfc7c710efebf4c40b8c4890b382655e0df
Seshien <maciek22999@wp.pl>
2021-04-25 17:09:37 +0200

    better parsing

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def main(argv):
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
     app.run(**args)
 
+#Very simple and lazy parse returning dict, searching for an argument and then treating next argv as its value
 def simpleParse(argv):
     result = {}
-    for it in ["--port", "--host", "--debug"]:
-        if it in argv:
-            result[it[2:]] = argv[argv.index(it) + 1]
+    handled_arg = ["--port", "--host", "--debug"]
+    for it in handled_arg:
+        try:
+            if it in argv:
+                index = argv.index(it)
+                if argv[index+1] not in handled_arg:
+                    result[it[2:]] = argv[index+1]
+        except IndexError as exc:
+            pass
     return result
 
 
